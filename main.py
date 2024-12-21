09050a64243f61c8ac632a597e3b1e9a540be6dd
Nasimjon28 <shahlo.reg@gmail.com>
2024-12-21 16:28:07 +0300

    kytj

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ dp = Dispatcher(bot, storage = MemoryStorage())
 @dp.message_handler(commands=['start'])
 async def start_message(message):
     print("Привет! Я бот помогающий твоему здоровью")
+    await message.answer("Привет! Я бот помогающий твоему здоровью"df)
 @dp.message_handler()
 async def all_message(message):
    print("Введите команду /start, чтобы начать общение")
+   await message.answer("Введи команду /start,что бы начать наше общение")
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
